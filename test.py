d52d11dc8456f53840860441538da0cd6dfeaa3b
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-11-16 19:42:04 +0700

    added new test file

--- test.py
+++ test.py
@@ -5,11 +5,13 @@ import math
 
 sourceText = """
 using System;
+
 if (0==1){
     int a = 0;
-    bool b = true; int asd = 5; string asd1 = "hello world";
+    bool d = true; int asd = 5; string asd1 = "hello world";
     if (1){
         Console.WriteLine("lol");
+        Console.Write("ban");
     }
     while (true){
         if (1){
@@ -20,16 +22,29 @@ if (0==1){
         }
         else{
             a = 0;
+            float b = Math.Cos(25) * Math.PI;
+            int c = new Random().Next(0, 1000);
+            double t = new Random().NextDouble();
+            if (!(true || false && 1)) { }
         }
         for (int i = 0; i < 5; i+=1){
             Console.WriteLine(i);
+            foreach (var lol in customArray){
+                var l = 0;
+                if (true){ }
+            }
         }
     }
+    interface ITest{
+        /* test interface.*/
+        void test();
+    }
+    int[] a = {1, 2, 3, 4};
 }
 """
 translator = cs2py.CSharpToPython(useRegex=1)
 print(translator.compile(sourceText))
-# sourceText = regex.sub(r"(?P<blockIndent>[ ]*)if[ ]*(?P<condition>[\S ]*){[\r\n]+(?P<body>(?P<indent>[ ]*)[^\r\n]+[\r\n]+((?P=indent)[^\r\n]+[\r\n]+)*)(?P=blockIndent)}",
-#     r"\g<blockIndent>if \g<condition>:\n\g<body>", sourceText)
+# sourceText = regex.sub(r"\n(?P<blockIndent>[ ]*)if[ ]*\((?P<condition>[\S ]*)\){[\r\n]+(?P<body>(?P<indent>[ ]*)[^\r\n]+[\r\n]+((?P=indent)[^\r\n]+[\r\n]+)*)(?P=blockIndent)}",
+#     r"\n\g<blockIndent>if \g<condition>:\n\g<body>", sourceText)
 # print(sourceText)
 # print(round(math.e))
