77671e5417dea7089f4b4885d0c3a4b1f06094a3
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-11-16 15:52:25 +0700

    added new rules :eyes:

--- cs2py/CSharpToPython.py
+++ cs2py/CSharpToPython.py
@@ -28,24 +28,39 @@ class CSharpToPython(Translator):
         # false
         # False
         (r"(?P<left>[\r\n]+(([^\"\r\n]*\"[^\"\r\n]+\"[^\"\r\n]*)+|[^\"\r\n]+))false", r"\g<left>False", None, 0),
+        # this
+        # self
+        (r"(?P<left>[\r\n]+(([^\"\r\n]*\"[^\"\r\n]+\"[^\"\r\n]*)+|[^\"\r\n]+))this", r"\g<left>self", None, 0),
         # // ...
         # # ...
         (r"//([^\r\n]+)", r"#\1",None, 0),
-        # for (...){
+        # for (int i = 0; i < 5; i++){
         #     ....
         # }
-        # for ... in ...:
+        # for i in range(0, 5, 1):
         #     ....
         (r"(?P<blockIndent>[ ]*)for[ ]*\((?P<varType>[\S]+)[ ]*(?P<varName>[\S]+)[ ]*=[ ]*(?P<variable>[\S]+)[ ]*;[ ]*(?P=varName)[ ]*[\S]+[ ]*(?P<number>[\S]+)[ ]*;[ ]*(?P=varName)[ ]*(\+=|\-=|\+\+|\-\-)[ ]*(?P<number2>[\S]+)[ ]*\){[\r\n]+(?P<body>(?P<indent>[ ]*)[^\r\n]+[\r\n]+((?P=indent)[^\r\n]+[\r\n]+)*)(?P=blockIndent)}", r'\g<blockIndent>for \g<varName> in range(\g<variable>, \g<number>, \g<number2>):\n\g<body>', None, 70),
+        # foreach (var i in array){
+        #     ....
+        # }
+        # for i in array:
+        #     ....
+        (r"(?P<blockIndent>[ ]*)foreach[ ]*\((?P<varType>[\S]+)[ ]*(?P<varName>[\S]+)[ ]*in[ ]*(?P<array>[\S]+)\){[\r\n]+(?P<body>(?P<indent>[ ]*)[^\r\n]+[\r\n]+((?P=indent)[^\r\n]+[\r\n]+)*)(?P=blockIndent)}", r'\g<blockIndent>for \g<varName> in \g<array>:\n\g<body>', None, 70),
         # ;\n
         # \n
         (r"(?P<indent>[ ]*)(?P<line>[\S \t]*);\n", r"\g<indent>\g<line>\n",None, 0),
         # ;
         # \n
-        (r"(?P<indent>[ ]*)(?P<line>[\S \t]*);[^\r\n]*;", r"\g<indent>\g<line>\n\g<indent>",None, 10),
+        (r"(?P<indent>[ ]*)(?P<line>[\S \t]*);[^\r\n]*;", r"\g<indent>\g<line>\n\g<indent>",None, 0),
         # ;
         # \n
-        (r"(?P<indent>[ ]*)(?P<line>[\S \t]*);[^\r\n]*#", r"\g<indent>\g<line> #",None, 10),
+        (r"(?P<indent>[ ]*)(?P<line>[\S \t]*);[^\r\n]*#", r"\g<indent>\g<line> #",None, 0),
+        # int i = 0;
+        # i = 0;
+        (r"(?P<blockIndent>[ ]*)(?P<varType>[\S]+)[ ]*(?P<varName>[a-zA-Z0-9_]+)[ ]*=", r'\g<blockIndent>\g<varName> =',None, 0),
+        # int[] i = {1, 2, 3};
+        # i = [1, 2, 3];
+        (r"(?P<blockIndent>[ ]*)(?P<varName>[a-zA-Z0-9_]+)[ ]*=[ ]*{(?P<list>[\S ]+)}", r'\g<blockIndent>\g<varName> = [\g<list>]',None, 0),
         # /* ... */
         # """ ... """
         (r"/\*(?P<comment>[\S\s]+)\*/", r'"""\g<comment>"""',None, 0),
@@ -73,8 +88,19 @@ class CSharpToPython(Translator):
         # while ...:
         #     ....
         (r"(?P<blockIndent>[ ]*)while[ ]*\((?P<condition>[\S ]*)\){[\r\n]+(?P<body>(?P<indent>[ ]*)[^\r\n]+[\r\n]+((?P=indent)[^\r\n]+[\r\n]+)*)(?P=blockIndent)}", r'\g<blockIndent>while \g<condition>:\n\g<body>', None, 70),
+        # interface IInterface{
+        #     ....
+        # }
+        # class IInterface:
+        #     ....
+        (r"(?P<blockIndent>[ ]*)interface[ ]*(?P<interfaceName>[a-zA-Z0-9_]+)[ ]*{[\r\n]+(?P<body>(?P<indent>[ ]*)[^\r\n]+[\r\n]+((?P=indent)[^\r\n]+[\r\n]+)*)(?P=blockIndent)}", r'\g<blockIndent>class \g<interfaceName>:\n\g<body>', None, 70),
+        # void test();
+        # def test():
+        #     pass
+        (r"(?P<start>[\r\n]+)(?P<blockIndent>[ ]*)(?P<returnType>\w+)[ ]+(?P<methodName>\w+)[ ]*\((?P<args>[\S ]*)\)", r'\g<start>\g<blockIndent>def \g<methodName>(\g<args>):\n\g<blockIndent>    pass', None, 0),
         # garbage delete
-        (r"\n\n", r"\n", None, 0)
+        (r"\n\n", r"\n", None, 0),
+        (r"(?P<blockIndent>[ ]*)(?P<other>[\S ]*){[\s]*}", r"\g<blockIndent>\g<other>:\n\g<blockIndent>    pass", None, 0)
     ]
 
     LAST_RULES = []
